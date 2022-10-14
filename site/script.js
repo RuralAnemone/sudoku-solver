d6ef2559eab692e7e4ca27b48d303a90641d8b63
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2022-10-14 19:25:52 +0000

    uh uh uh uh uh uh uh uh uh uh uh uh uh uh uh

--- site/script.js
+++ site/script.js
@@ -1,21 +1,29 @@
 // https://www.desmos.com/calculator/q1i5i4mnju
 
 // variables
-var backtrack = [];
+// initialize with sample board
+var backtrack = [0,3,0,9,8,4,2,0,0,0,0,9,0,0,7,0,0,3,8,0,0,0,0,0,9,0,0,0,0,6,0,0,2,0,1,0,2,0,0,7,0,5,6,3,0,0,0,0,0,9,0,4,0,8,0,6,2,0,0,0,5,0,0,1,0,0,0,0,0,0,7,0,3,0,4,0,6,1,0,0,0];
+var sudoku = [0,3,0,9,8,4,2,0,0,0,0,9,0,0,7,0,0,3,8,0,0,0,0,0,9,0,0,0,0,6,0,0,2,0,1,0,2,0,0,7,0,5,6,3,0,0,0,0,0,9,0,4,0,8,0,6,2,0,0,0,5,0,0,1,0,0,0,0,0,0,7,0,3,0,4,0,6,1,0,0,0];
+var solved = false;
 
-function solve() {
-  
+// main
+
+function solveSudoku() {
+  setInterval(() => {
+    solved ? 0 : cLogic();
+  },200)
 }
-
 
 // reset
 
 function reset() {
+  console.log("reset");
   backtrack = sudoku;
   updateGrid();
 }
 
 function clear() {
+  console.log("clear");
   backtrack = [];
   for (let i = 0; i < 81; i++) {
     console.log(backtrack);
@@ -23,3 +31,8 @@ function clear() {
   }
   console.log(backtrack);
 }
+
+// grid
+// oh boy
+
+let tSize
